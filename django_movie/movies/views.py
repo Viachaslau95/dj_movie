b1e9bb1f4282d7e9fba8ea19ec606e888f006382
Viachaslau95 <6891094@mail.ru>
2021-05-12 23:22:31 +0300

    добавил форму по добавлению отзывов к фильму.

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
+
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -17,3 +19,16 @@ class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
 
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get('parent', None):
+                form.parent_id = int(request.POST.get('parent'))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
